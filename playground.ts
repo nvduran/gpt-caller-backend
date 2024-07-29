ed0121e3b875783355ae97ee22be8f3d60a8dfba
nvduran <nvduran@gmail.com>
2024-07-28 22:57:43 -0500

    custom mesage

--- playground.ts
+++ playground.ts
@@ -10,7 +10,18 @@ async function createCall() {
   const call = await client.calls.create({
     from: "+18888799239",
     to: "+15126567738",
-    url: "http://demo.twilio.com/docs/voice.xml",
+    twiml: `
+      <Response>
+        <Start>
+          <Stream url="wss://localhost:8080"/>
+        </Start>
+        <Say>Hello, this call is being transcribed.</Say>
+        <Pause length="60"/>
+        <Stop>
+          <Stream />
+        </Stop>
+      </Response>
+    `,
   });
 
   console.log(call.sid);
